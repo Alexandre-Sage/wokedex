4c0f6bbf57e3390900f82aaa716cedcf6a791223
Alexandre Sage <alexandre@adsoftware.fr>
2023-10-12 20:24:27 +0200

    feat: docker front

--- apps/frontend/react-app/src/components/wokemons/createWokemon/WokemonForm.tsx
+++ apps/frontend/react-app/src/components/wokemons/createWokemon/WokemonForm.tsx
@@ -1,5 +1,5 @@
-import { Button, HStack, VStack } from "@chakra-ui/react";
-import { useState } from "react";
+import { Button, HStack, VStack, Stack } from "@chakra-ui/react";
+import { ReactNode, useState } from "react";
 import { theme } from "../../../App";
 import { createWokemon, createWokemonImage } from "../../../api/wokemon.api";
 import { WokemonPayload } from "../../../types/Wokemon.type";
@@ -11,6 +11,19 @@ export const Attack = () => {
   const { refetch, attacks } = useAttacks();
   return <VStack>HELLO</VStack>;
 };
+
+export const FormSectionLayout = ({ children }: { children: ReactNode }) => (
+  <VStack
+    bgColor={theme.config.colors.darkGrey}
+    borderRadius={5}
+    h={{ xl: "xl", md: "md", sm: "md",base:"md" }}
+    w={{ xl: "xl", md: "md", sm: "sm",base:"xs" }}
+    alignItems={"center"}
+    justifyContent={"center"}
+  >
+    {children}
+  </VStack>
+);
 const CreateWokemonForm = () => {
   const [newWokemon, setNewWokemon] = useState<WokemonPayload>({
     types: [] as string[],
@@ -41,32 +54,32 @@ const CreateWokemonForm = () => {
         image: f,
       };
     });
-  console.log({ newWokemon });
   return (
-    <VStack>
-      <HStack>
-        <VStack>
-          <WokemonFormRightCol
-            onFormChange={onFormChange}
-            setImage={setWokemonImage}
-          />
-        </VStack>
-        <VStack
-          bgColor={theme.config.colors.darkGrey}
-          borderRadius={5}
-          w="xl"
-          h="xl"
-          alignItems={"center"}
-          justifyContent={"center"}
-        >
+    <VStack
+      alignItems={{ base: "center" }}
+      justify={{ xl: "center", md: "center", sm: "flex-end" }}
+      direction={"column"}
+    >
+      <Stack
+        border={"1px solid red"}
+        alignItems={{ base: "center" }}
+        justify={{ xl: "center", md: "center", sm: "flex-end" }}
+        direction={{ xl: "row", md: "row", sm: "column", base: "column" }}
+        h={{ xl: "3xl", md: "xl", sm: "4xl",base:"4xl" }}
+      >
+        <WokemonFormRightCol
+          onFormChange={onFormChange}
+          setImage={setWokemonImage}
+        />
+        <FormSectionLayout>
           <HStack>
             <TypeDragAndDrop onChange={(e) => onFormChange("types", e)} />
           </HStack>
-          <HStack border={"1px solid red"} h="60" w="sm">
+          <HStack /* border={"1px solid red"} h="60" w="sm" */>
             <AttackDragAndDrop onChange={(e) => onFormChange("attacks", e)} />
           </HStack>
-        </VStack>
-      </HStack>
+        </FormSectionLayout>
+      </Stack>
       <HStack>
         <Button
           w={"4xl"}
